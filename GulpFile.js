4f4a3daa239fb62555cd5f1888beaee38f2c52d4
John Lindquist <johnlindquist@gmail.com>
2014-07-29 14:40:10 -0600

    first commit

---
+++ GulpFile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
+var connect = require('gulp-connect');
+
+var server = require('./server/server');
+var open = require('open');
+
+gulp.task('browserify', function() {
+    gulp.src(['./client/src/app.js'])
+        .pipe(browserify({
+            insertGlobals: true,
+            debug: true
+        }))
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./client/dist'));
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: './client/dist',
+        livereload: true
+    });
+});
+
+gulp.task('html', function () {
+    gulp.src('./client/dist/index.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', [], function() {
+    gulp.watch(['client/src/app.js'],[
+        'browserify',
+        'html'
+    ]);
+});
+
+gulp.task('server', function () {
+    server();
+})
+
+
+gulp.task('default', ['connect', 'server'], function () {
+    open("http://localhost:8080")
+})
---
+++ README.md
@@ -0,0 +1,8 @@
+npm install
+gulp
+
+
+
+
+Runs a "Board Game" server with a SQLite database and board game images.
+The angular "app" is a minimal example of requesting the data.
