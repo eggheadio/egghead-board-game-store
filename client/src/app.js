c0e6867bab55d7e0124d2a956d567f16445c8d64
John Lindquist <johnlindquist@gmail.com>
2014-07-30 13:31:02 -0600

    removing dist from repo

--- client/src/app.js
+++ client/src/app.js
@@ -1,17 +1,24 @@
-var angular = require("angular");
-angular.module("bgs", [])
-    .constant("BASE_URL", "http://localhost:3000")
-    .service("boardGameService", function ($http, BASE_URL) {
+!function () {
+    var BASE_URL = "http://localhost:3000";
+
+    function boardGameService($http, BASE_URL) {
         this.getGames = function () {
             return $http.get(BASE_URL + "/game")
         }
-    })
+    }
 
-    .controller("AppCtrl", function (boardGameService) {
+    function AppCtrl(boardGameService) {
         var app = this;
         app.games = [];
+
         boardGameService.getGames()
             .then(function (result) {
                 app.games = result.data;
             })
-    })
+    }
+
+    require("angular").module("bgs", [])
+        .constant("BASE_URL", BASE_URL)
+        .service("boardGameService", boardGameService)
+        .controller("AppCtrl", AppCtrl)
+}();
